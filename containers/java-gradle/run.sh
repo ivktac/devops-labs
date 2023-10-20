c25f38be6f233b8ab69917fc883d9092405ffbc5
Ivan Tkachuk <116971836+hxrv0q@users.noreply.github.com>
2023-10-20 21:12:28 +0300

    Add dockerfile for java with gradle

---
+++ containers/java-gradle/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# if exists docker use instead of podman
+
+if command -v docker &> /dev/null
+then
+    echo "docker exists"
+    alias podman=docker
+fi
+
+podman build -t my-java-app .
+
+podman run my-java-app
